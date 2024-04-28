8f7f9e24becde822dae437c91e087d3a0da3ad6a
kevinhugg <kevinmar704@gmail.com>
2024-04-28 17:01:06 -0300

    first commit

---
+++ sla.js
@@ -0,0 +1,11 @@
+function autenticar() {
+    let usuario= document.getElementById("login").value;
+    //let usuario= document.querySelector("login").value;
+    let senha= document.getElementById("senha").value;
+
+    if(usuario==="usuario" && senha=== "123"){
+        document.getElementById("mensagem").innerHTML="Usuário autenticado.";
+    } else {
+        document.getElementById("mensagem").innerHTML="Autenticação inválida.";
+    }
+}
